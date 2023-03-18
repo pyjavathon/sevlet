8f5beadf792144b020e04d45493cb21668c77310
pyjavathon <86497379+pyjavathon@users.noreply.github.com>
2023-03-18 22:57:32 +0900

    1

---
+++ src/main/java/hello/sevlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,35 @@
+package hello.sevlet.basic.response;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import hello.sevlet.basic.HelloData;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet{
+
+	private ObjectMapper objectMapper = new ObjectMapper();
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		response.setContentType("application/json");
+		response.setCharacterEncoding("utf-8");
+		
+		HelloData helloData = new HelloData();
+		helloData.setUsername("u");
+		helloData.setAge("20");
+		
+		String result = objectMapper.writeValueAsString(helloData);
+		
+		response.getWriter().write(result);
+		
+	}
+}
