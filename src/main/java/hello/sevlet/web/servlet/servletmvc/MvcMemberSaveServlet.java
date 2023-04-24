6b4e1c37628624f79b7cbde498bb252d595f694d
pyjavathon <86497379+pyjavathon@users.noreply.github.com>
2023-04-24 23:51:46 +0900

    서블릿, jsp, mvc 패턴 적용해보기

---
+++ src/main/java/hello/sevlet/web/servlet/servletmvc/MvcMemberSaveServlet.java
@@ -0,0 +1,36 @@
+package hello.sevlet.web.servlet.servletmvc;
+
+import java.io.IOException;
+
+import hello.sevlet.domain.member.Member;
+import hello.sevlet.domain.member.MemberRepository;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "mvcMemberSaveServlet", urlPatterns = "/servlet-mvc/members/save")
+public class MvcMemberSaveServlet extends HttpServlet{
+	
+	private MemberRepository memberRepository = MemberRepository.getInstance();
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String username = req.getParameter("username");
+		int age = Integer.parseInt(req.getParameter("age"));
+		
+		Member member = new Member(username,age);
+		memberRepository.save(member);
+	
+		//Model에 데이터를 보관한다.
+		req.setAttribute("member", member);
+		
+		String viewPath = "/WEB-INF/views/save-result.jsp";
+		RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+		dispatcher.forward(req, resp);
+	}
+
+}
